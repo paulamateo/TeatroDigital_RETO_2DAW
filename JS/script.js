5c1e56ccbd5ef02590428c02c5570908d1757d45
Paula Mateo <a26619@svalero.com>
2023-12-01 15:46:13 +0100

    [FEATURE-HOMEPAGE] Page finished (without Sass)

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,20 @@
 document.addEventListener('scroll', function() {
-    var header = document.querySelector('header');
+    var header = document.getElementById('header');
+    var headerPopup = document.getElementById('header__popup');
     var logoImage = document.getElementById('logoImage');
+    var searchContainer = document.getElementById('search');
     var scrollPosition = window.scrollY;
 
     if (scrollPosition > 50) {
         header.classList.add('scrolled');
-        logoImage.src = '../IMÁGENES/logo_blanco.png';
+        logoImage.src = '../IMG/logo_blanco.png';
+        searchContainer.classList.add('scrolled');
+        headerPopup.classList.add('scrolled');
     } else {
         header.classList.remove('scrolled');
-        logoImage.src = '../IMÁGENES/logo_negro.png';
+        logoImage.src = '../IMG/logo_negro.png';
+        searchContainer.classList.remove('scrolled');
+        headerPopup.classList.remove('scrolled');
     }
 });
 
@@ -19,4 +25,9 @@ document.addEventListener('DOMContentLoaded', function() {
     hamburgerIcon.addEventListener('click', function() {
         headerPopup.classList.toggle('active');
     });
+});
+
+document.getElementById('chevronleft-icon').addEventListener('click', function() {
+    var showsSection = document.querySelector('.shows');
+    showsSection.scrollIntoView({ behavior: 'smooth' });
 });
