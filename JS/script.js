99a2ba4082d501ed195d3f15751e2702733359dc
Paula Mateo <a26619@svalero.com>
2023-12-02 04:27:44 +0100

    [RESERVETICKETS] Added menu, footer & others

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,33 @@
 document.addEventListener('scroll', function() {
-    var header = document.querySelector('header');
+    var header = document.getElementById('header');
+    var headerPopup = document.getElementById('header__popup');
     var logoImage = document.getElementById('logoImage');
+    var searchContainer = document.getElementById('search');
     var scrollPosition = window.scrollY;
 
     if (scrollPosition > 50) {
         header.classList.add('scrolled');
         logoImage.src = '../IMÁGENES/logo_blanco.png';
+        searchContainer.classList.add('scrolled');
+        headerPopup.classList.add('scrolled');
     } else {
         header.classList.remove('scrolled');
         logoImage.src = '../IMÁGENES/logo_negro.png';
+        searchContainer.classList.remove('scrolled');
+        headerPopup.classList.remove('scrolled');
     }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var hamburgerIcon = document.getElementById('list-icon');
+    var headerPopup = document.getElementById('header__popup');
+
+    hamburgerIcon.addEventListener('click', function() {
+        headerPopup.classList.toggle('active');
+    });
+});
+
+document.getElementById('chevronleft-icon').addEventListener('click', function() {
+    var showsSection = document.querySelector('.shows');
+    showsSection.scrollIntoView({ behavior: 'smooth' });
 });
